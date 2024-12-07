249c0197ae0dc8f6c37cd02b4c7db5e035df98e4
Ogaday Willers Moore <W.Ogaday@gmail.com>
2024-12-07 00:32:48 +0000

    Feat: Add day 03 solutions

---
+++ aoc/solutions/day_03/part_1.py
@@ -0,0 +1,22 @@
+import re
+
+
+prog = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+
+def clean(memory: str) -> list[list[int]]:
+    """
+    Examples
+    --------
+    >>> clean("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+    [(2, 4), (5, 5), (11, 8), (8, 5)]
+    """
+    return [tuple(map(int, match.groups())) for match in prog.finditer(memory)]
+
+
+def solution(path):
+    with open(path) as f:
+        memory = f.read()
+
+    factors = clean(memory=memory)
+    return sum(a * b for a, b in factors)
---
+++ aoc/solutions/day_03/part_2.py
@@ -0,0 +1,22 @@
+import re
+
+
+prog = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|(don't\(\))|(do\(\))")
+
+
+def solution(path):
+    with open(path) as f:
+        memory = f.read()
+
+    enabled = True
+    factors = []
+    for match in prog.finditer(memory):
+        a, b, dont, do = match.groups()
+        if dont:
+            enabled = False
+        elif do:
+            enabled = True
+        elif enabled and a and b:
+            factors.append((int(a), int(b)))
+
+    return sum(a * b for a, b in factors)
